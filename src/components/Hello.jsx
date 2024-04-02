5fd8ae9c0c12e8a8cfe1b430e43fab6f2de55296
Miraflor <wmiraflor@hartmannph.com>
2024-04-02 18:03:42 +0800

    testing

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,12 +1,32 @@
-function Hello({ person }) {
+export default function Hello({ person }) {
   return (
-    <div>
-      <h1>
-        {person.message} {person.emoji} {person.name} {person.seatNumbers}
-        {" test 2 "}
-      </h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3>Column 1</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+          <p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris...
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Column 2</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+          <p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris...
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Column 3</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+          <p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris...
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Hello;
